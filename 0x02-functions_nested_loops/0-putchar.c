9e7b959b456a20ef083bb1b911fbe89fe1b87bd1
Quintony1 <nyashajerahuni@gmail.com>
2022-06-07 14:55:52 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,29 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: nothing, return void
  */
+
 int main(void)
 {
-	char *sh = "Main";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	 _putchar('t');
+	  _putchar('c');
+	  _putchar('h');
+	  _putchar('a');
+	  _putchar('r');
+	  _putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 	return (0);
+return (0);
 }
