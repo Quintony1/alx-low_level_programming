82f1deb02854ca42a6923e0ca732401fdcbcf2a9
Quintony1 <nyashajerahuni@gmail.com>
2022-06-27 14:47:04 -0700

    1-strdup.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,6 +5,7 @@
  * create_array - Creates an array of chars and
  *                initializes it with a specific char.
  * @size: The size of the array to be initialized.
+ * @c: The specific char to intialize the array with.
  *
  * Return: If size == 0 or the function fails - NULL.
  *         Otherwise - a pointer to the array.
@@ -27,3 +28,6 @@ char *create_array(unsigned int size, char c)
 
 	return (array);
 }
+© 2022 Gi
+
+
